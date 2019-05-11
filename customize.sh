838bea5c0474a629609b929e74a85c773ee10977
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2019-05-11 22:55:47 +0200

    fixes, cleanup

--- customize.sh
+++ customize.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# remove packages
+sed -i '|grml-zsh-config|d' packages.x86_64
+
 # add packages
 cat << EOF >> packages.x86_64
 pacman-contrib
@@ -27,11 +30,11 @@ EOF
 # enable pacman colors
 sed -i 's|^#Color|Color|' pacman.conf
 
-# change autologin user
-sed -i 's|isouser|nicoulaj|' airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf
-
 # set keymap
 echo KEYMAP=fr > airootfs/etc/vconsole.conf
 
 # dotfiles
 curl -Ls https://github.com/nicoulaj/dotfiles/archive/master.tar.gz | tar xvz -C airootfs/root/ --strip-components=1
+
+# FIXME lz4 compression for initramfs
+# sed 's|#COMPRESSION="lz4"|COMPRESSION="lz4"|' airootfs/etc/mkinitcpio.conf
